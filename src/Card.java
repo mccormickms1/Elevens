ca02b1b43b7e639b0cf8b94fa3c8a05099d6edd9
Matthew Stephen McCormick <mccormickms@s.dcsdk12.org>
2019-04-01 08:05:20 -0600

    a1

--- src/Card.java
+++ src/Card.java
@@ -32,7 +32,9 @@ public class Card {
 	 *                  containing the point value of the card
 	 */
 	public Card(String cardRank, String cardSuit, int cardPointValue) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		rank = cardRank;
+		suit = cardSuit;
+		pointValue = cardPointValue;
 	}
 
 
@@ -41,15 +43,15 @@ public class Card {
 	 * @return this <code>Card's</code> suit.
 	 */
 	public String suit() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
-   }
+		return suit;
+   	}
 
 	/**
 	 * Accesses this <code>Card's</code> rank.
 	 * @return this <code>Card's</code> rank.
 	 */
 	public String rank() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return rank;
 	}
 
    /**
@@ -57,7 +59,7 @@ public class Card {
 	 * @return this <code>Card's</code> point value.
 	 */
 	public int pointValue() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return pointValue;
 	}
 
 	/** Compare this card with the argument.
@@ -67,7 +69,10 @@ public class Card {
 	 *         false otherwise.
 	 */
 	public boolean matches(Card otherCard) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		if (otherCard.suit().equals(suit) && otherCard.rank().equals(rank)) {
+			return true;
+		}
+		return false;
 	}
 
 	/**
@@ -82,6 +87,6 @@ public class Card {
 	 */
 	@Override
 	public String toString() {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		return rank + " of " + suit + " " + String.valueOf(pointValue);
 	}
 }
--- src/CardTester.java
+++ src/CardTester.java
@@ -8,6 +8,14 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card mark = new Card("King", "Diamonds", 10);
+		Card susan = new Card("King", "Diamonds", 10);
+		Card dad = new Card("Five", "Spades", 5);
+		System.out.println(mark.rank());
+		System.out.println(mark.suit());
+		System.out.println(mark.pointValue());
+		System.out.println(mark.matches(susan));
+		System.out.println(mark.matches(dad));
+		System.out.println(mark.toString());
 	}
 }
